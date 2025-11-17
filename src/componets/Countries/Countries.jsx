f7878246219681afad1f1963627d18919284f99e
Rajib Reza <rajibreza64@gmail.com>
2025-11-17 10:37:42 +0600

    add img tag for flag

--- src/componets/Countries/Countries.jsx
+++ src/componets/Countries/Countries.jsx
@@ -1,18 +1,21 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = ({promiseCountries}) => {
     const countries = use(promiseCountries);
     console.log(countries);
     return (
-        <div>
-            <h1>This is All Api Ai data For Country :  {countries.length}</h1>
+     <div>
+        <h1>This is All Api Ai data For Country :  {countries.length}</h1>
+           <div className='countries-container'>
 
             {
                countries.map(country => <Country country={country}></Country>)
             }
             
         </div>
+     </div>
     );
 };
 
--- src/componets/Country/Country.jsx
+++ src/componets/Country/Country.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Countries from '../Countries/Countries';
+import './Country.css'
 
-const Country = ({country}) => {
-    console.log(country);
+const Country = ({ country }) => {
     return (
-        <div>
-             <h2>Country Name: {country.name.common}</h2>
+        <div className='country'>
+            <h2>Country Name: {country.name.common}</h2>
+
+            {/* h2 er niche flag image */}
+            <img 
+                className="flag"
+                src={country.flags.png} 
+                alt={country.name.common}
+            />
+
             <p><strong>Official Name:</strong> {country.name.official}</p>
             <p><strong>Capital:</strong> {country.capital?.[0] || "No Capital"}</p>
             <p><strong>Region:</strong> {country.region}</p>
