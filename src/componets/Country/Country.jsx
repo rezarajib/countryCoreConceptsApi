49225bc1f6a1581d59fae3fc4fd919d5db74cbef
Rajib Reza <rajibreza64@gmail.com>
2025-11-17 13:00:48 +0600

    add visited btn

--- src/componets/Country/Country.jsx
+++ src/componets/Country/Country.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
-import './Country.css';
+import Countries from '../Countries/Countries';
+import './Country.css'
 
 const Country = ({ country }) => {
 
     const [visited, setVisited] = useState(false);
-
     const handleBtn = () => {
-        setVisited(!visited);   // toggle true/false
+        setVisited (true);
     }
-
     return (
         <div className='country'>
             <h2>Country Name: {country.name.common}</h2>
@@ -24,18 +23,9 @@ const Country = ({ country }) => {
             <p><strong>Capital:</strong> {country.capital?.[0] || "No Capital"}</p>
             <p><strong>Region:</strong> {country.region}</p>
             <p><strong>Population:</strong> {country.population}</p>
-
-            {/* visited text */}
-            <p className='visit-status'>
-                Status: {visited ? "Visited" : "Not Visited"}
-            </p>
-
-            <button 
-                className='visit-btn'
-                onClick={handleBtn}
-            >
-                {visited ? "Mark as Not Visited" : "Mark as Visited"}
-            </button>
+            <button onClick={handleBtn}> {
+                visited ? 'visited' : 'Not Visited'
+                } </button>
         </div>
     );
 };
